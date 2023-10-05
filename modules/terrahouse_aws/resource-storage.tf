7a2b967fec5606b8dec72a4f9ede2aee4e3081a6
Victor N <victorkjngo@gmail.com>
2023-10-05 14:12:33 -0700

    #29 Create CDN and serve S3 bucket contents from CDN (#30)

---
+++ modules/terrahouse_aws/resource-storage.tf
@@ -0,0 +1,69 @@
+
+resource "aws_s3_bucket" "website_bucket" {
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = var.bucket_name
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
+# Create an S3 bucket for your static website
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+  content_type = "text/html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  // Data isn't checked!
+  source = var.error_html_filepath
+  content_type = "text/html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.error_html_filepath)
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+    policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": {
+            "Sid": "AllowCloudFrontServicePrincipalReadOnly",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudfront.amazonaws.com"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+            "Condition": {
+              "StringEquals": {
+                "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+                  # "AWS:SourceArn": aws_cloudfront_distribution.s3_distribution.arn
+              }
+            }
+        }
+    })
+}
