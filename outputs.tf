7336a61537811ad0eab76b7c14c5cc6a0c1499de
V N <victorkjngo@gmail.com>
2023-10-06 01:52:42 +0000

    Fix path for etag Pass through assets path var into nested module

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,13 @@ output "bucket_name" {
   value = module.terrahouse_aws.bucket_name
 }
 
+output "cloudfront_url" {
+  description = "The Cloudfront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
+
 output "s3_website_endpoint" {
   description = "S3 Static Website Hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
 
-output "cloudfront_url" {
-  description = "The Cloudfront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
